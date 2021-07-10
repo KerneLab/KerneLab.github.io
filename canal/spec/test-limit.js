6a0f0746e4fa5639da1f6ff488044970e6d5dfff
核心力量 <kernelforce@gmail.com>
2021-07-10 14:13:27 +0800

    Update with canal.

--- canal/spec/test-limit.js
+++ canal/spec/test-limit.js
@@ -10,6 +10,20 @@ it("limit(2) 1 2 3", function()
 	expect(result).to.eql([ 1, 2 ]);
 });
 
+it("limit(null) 1 2 3", function()
+{
+	var result = Canal.of([ 1, 2, 3 ]) //
+	.limit(null).collect();
+	expect(result).to.eql([ 1, 2, 3 ]);
+});
+
+it("limit(-1) 1 2 3", function()
+{
+	var result = Canal.of([ 1, 2, 3 ]) //
+	.limit(-1).collect();
+	expect(result).to.eql([ 1, 2, 3 ]);
+});
+
 it("limit(3) 1 2 3", function()
 {
 	var result = Canal.of([ 1, 2, 3 ]) //
